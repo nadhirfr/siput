f5d825a21506b89ede70e2eda0ca0cb38f00c6a7
fachrul <fachrul@DESKTOP-48P7DQ9>
2017-03-11 17:50:27 +0700

    Fix koneksi database

--- src/koneksi/Koneksi.java
+++ src/koneksi/Koneksi.java
@@ -10,13 +10,17 @@ import java.sql.SQLException;
 public class Koneksi {
 
     static Connection con;
+    private final static String DB_HOST = "localhost";
+    private final static String DB_NAME = "siput";
+    private final static String DB_USER = "root";
+    private final static String DB_PASS = "";
 
     public static Connection connection() {
         if (con == null) {
             MysqlDataSource data = new MysqlDataSource();
-            data.setDatabaseName("pl9");
-            data.setUser("root");
-            data.setPassword(null);
+            data.setDatabaseName(DB_NAME);
+            data.setUser(DB_USER);
+            data.setPassword(DB_PASS);
             try {
                 con = data.getConnection();
             } catch (SQLException ex) {
@@ -30,7 +34,11 @@ public class Koneksi {
 
         try {
             Class.forName("com.mysql.jdbc.Driver");  // MySQL database connection
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/siput?" + "user=root&password=");
+            Connection conn = DriverManager.
+                                getConnection("jdbc:mysql://"+DB_HOST+":3306/"
+                                        +DB_NAME+"?"
+                                        +"user="+DB_USER
+                                        +"&password="+DB_PASS);
             PreparedStatement pst = conn.prepareStatement("select * from user where user_username=? and user_password=?");
             pst.setString(1, user);
             pst.setString(2, pass);
