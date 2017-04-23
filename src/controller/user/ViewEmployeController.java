d9c66b5600f663395f401d7f58982c46214ae878
fachrul <fachrul@DESKTOP-48P7DQ9>
2017-04-23 23:38:46 +0700

    nambah centang iuran di menu update user

--- src/controller/user/ViewEmployeController.java
+++ src/controller/user/ViewEmployeController.java
@@ -8,11 +8,6 @@ package controller.user;
 //import Getway.UsersGetway;
 //import dataBase.DBConnection;
 
-import dao.implementDeposit;
-import dao.implementUser;
-import factory.DAOFactory;
-import factory.MySQLDAOFactory;
-import factory.RESTDAOFactory;
 import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -26,33 +21,35 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
-import javafx.scene.shape.Rectangle;
 //import media.userNameMedia;
 //import custom.*;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.sql.Blob;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Observer;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.EventHandler;
+import javafx.scene.control.cell.CheckBoxListCell;
 import javafx.scene.control.cell.MapValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.ImagePattern;
-import javafx.stage.FileChooser;
-
-import javax.imageio.ImageIO;
+import javafx.scene.paint.Color;
+import javafx.util.Callback;
 import model.Deposit;
 import model.DepositModel;
+import model.Iuran;
+import model.IuranModel;
+import model.IuranUser;
+import model.IuranUserModel;
 import model.User;
 import model.UserModel;
+import org.controlsfx.control.CheckListView;
 
 //import List.ListEmployee;
 //
@@ -120,6 +117,10 @@ public class ViewEmployeController implements Initializable {
 
     UserModel userModel = new UserModel();
     DepositModel depositModel = new DepositModel();
+    IuranModel iuranModel = new IuranModel();
+    IuranUserModel iuranUserModel = new IuranUserModel();
+    List<Iuran> listIuran;
+    List<IuranUser> listIuranUser;
     List<User> listUser;
     
     @FXML
@@ -128,12 +129,25 @@ public class ViewEmployeController implements Initializable {
     private TextField tfTipeUser;
     @FXML
     private TextField tfSaldo;
+    @FXML
+    private CheckListView<Iuran> lvSelectedIuran;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 //        // TODO
+        lvSelectedIuran.setItems(generateSelectedIuranData());
+//        lvSelectedIuran.getCheckModel().checkAll();
+        lvSelectedIuran.setCellFactory(lv -> new CheckBoxListCell<Iuran>(lvSelectedIuran::getItemBooleanProperty) {
+            @Override
+            public void updateItem(Iuran item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(item == null ? "" : item.getIuranNama());
+            }
+            
+            
+        });
         fillTable();
         btnDelete.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
@@ -167,12 +181,25 @@ public class ViewEmployeController implements Initializable {
                     depositModel.update(new Deposit(deposit.getDepositId(),
                             deposit.getUserId(),
                             Integer.valueOf(tfSaldo.getText())));
+                    for (Iuran iuran : lvSelectedIuran.getItems()){
+                        if(iuranUserModel.getByUserAndIuran(user, iuran).getIuranUserId() == 0 &&
+                                lvSelectedIuran.getCheckModel().isChecked(iuran)){
+                            System.out.println("pertama true");
+                            iuranUserModel.insert(new IuranUser(user.getUser_id(), iuran.getIuranId(), 0));
+                        } else if(iuranUserModel.getByUserAndIuran(user, iuran).getIuranUserId() !=0 &&
+                                !lvSelectedIuran.getCheckModel().isChecked(iuran)){
+                            System.out.println("kedua true");
+                            iuranUserModel.delete(String.valueOf(iuranUserModel.getByUserAndIuran(user, iuran).getIuranUserId()));
+                        }
+                    }
                     System.out.println("Updated user id: "+String.valueOf(user.getUser_id()));
                     System.out.println("Updated deposit id: "+String.valueOf(deposit.getDepositId()));
                     tblEmoyeeList.getSelectionModel().selectFirst();
                     //ini harusnya direfresh tampilan tapi belum bisa
                     tblEmoyeeList.refresh();
                     fillTable();
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION, "Data telah di update!\nSilahkan klik refresh untuk memperbarui data!", ButtonType.OK);
+                    alert.showAndWait();
                 }
             }
         });
@@ -204,6 +231,17 @@ public class ViewEmployeController implements Initializable {
             String value1 = listUser.get(i).getUser_displayname();
             dataRow.put(Column1MapKey, value1);
             dataRow.put(Column0MapKey, value0);
+            allData.add(dataRow);
+        }
+        return allData;
+    }
+    
+    private ObservableList<Iuran> generateSelectedIuranData(){
+        listIuran = iuranModel.getAll();
+        ObservableList<Iuran> allData = FXCollections.observableArrayList();
+        allData.removeAll(allData);
+        for (int i = 0; i < listIuran.size(); i++) {
+            Iuran dataRow = listIuran.get(i);
             allData.add(dataRow);
         }
         return allData;
@@ -240,6 +278,14 @@ public class ViewEmployeController implements Initializable {
         if (employeeList != null) {
             user = userModel.getUser(employeeList.get(Column0MapKey).toString());
             //usersGetway.selectedView(users);
+            lvSelectedIuran.getCheckModel().clearChecks();
+            for (Iuran iuran : lvSelectedIuran.getItems()) {
+                if(iuranUserModel.getByUserAndIuran(user, iuran).getIuranId() == iuran.getIuranId()){
+                    lvSelectedIuran.getCheckModel().check(iuran);
+                } else{
+                    lvSelectedIuran.getCheckModel().clearCheck(iuran);
+                }
+            }
             id = String.valueOf(user.getUser_id());
             tfUserName.setText(user.getUser_username());
             tfFullName.setText(user.getUser_displayname());
