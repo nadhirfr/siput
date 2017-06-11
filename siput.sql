16fadf822c483f2a895e27c4c28e92d08959e87e
fachrul <fachrul@DESKTOP-48P7DQ9>
2017-06-11 21:04:45 +0700

    database fix

--- siput.sql
+++ siput.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2017 at 02:00 PM
+-- Generation Time: Jun 11, 2017 at 02:04 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
